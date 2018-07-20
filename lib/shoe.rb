f8d7618860e3465ebcb4f5b0c2f1ca8c6a8622bb
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-20 18:37:25 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,10 @@
-class Shoe 
+class Shoe
 
   attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
-    @brand = brand 
-  end 
+    @brand = brand
+  end
 
 
 end 
